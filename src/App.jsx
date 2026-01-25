9c4c7a75e8502eef0793d20d8a4b92851a16e6c9
IZaman123 <257001296+IZaman123@users.noreply.github.com>
2026-01-25 19:05:33 +0000

    Create page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const loggedInUser = "User";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="layout">
+            <header>
+                <h1>Basic React Demo</h1>
+                <p className="welcome">Welcome {loggedInUser}!</p>
+            </header>
+
+            <nav>
+                <div className="navItem">
+                    <a to="/">Home</a>
+                </div>
+
+                <div className="navItem">
+                    <a to="/modules">Modules</a>
+                </div>
+
+                <div className="navItem">
+                    <a to="/students">Students</a>
+                </div>
+            </nav>
+
+            <main>
+                <p>Homepage</p>
+            </main>
+
+            <footer>
+                <p className="thankyou">Thank you for using this system!</p>
+            </footer>
+        </div>
+    );
 }
 
-export default App
+export default App;
