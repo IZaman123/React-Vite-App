bd54cb7b83969910fd453e9b936ecc4c553791d1
IZaman123 <257001296+IZaman123@users.noreply.github.com>
2026-02-12 10:55:30 +0000

    Routing between views

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./components/views/Home";
 import Modules from "./components/views/Modules";
 import Students from "./components/views/Students";
+import PageNotFound from "./components/views/404";
 
 function App() {
     const loggedInUser = "User";
 
     return (
-        <Layout loggedInUser={loggedInUser}>
-            <Home />
-            <Modules />
-            <Students />
-        </Layout>
+        <BrowserRouter>
+            <Layout loggedInUser={loggedInUser}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/modules" element={<Modules />} />
+                    <Route path="/students" element={<Students />} />
+                    <Route path="/*" element={<PageNotFound />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
     );
 }
 
